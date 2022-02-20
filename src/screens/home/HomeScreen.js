7ba325651144776ad2f072d02c4e420644625c53
Nirav Malsattar <niravmalsattar@gmail.com>
2022-02-20 01:18:12 +0100

    fix loading more than 20 movies

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, View, FlatList } from 'react-native';
-import MCard from '../../components/mCard/MCard';
-import { API_URL, API_KEY } from '../../utils/constants';
-import { styles } from './styles';
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, FlatList, Alert } from "react-native";
+import MCard from "../../components/mCard/MCard";
+import { API_URL, API_KEY } from "../../utils/constants";
+import { styles } from "./styles";
 // import FetchMovieData from "../../services/api";
+
+let TOTAL_PAGE = 0;
+let CURR_PAGE = 1;
+let stopFetchMore = true;
 
 export default function HomeScreen() {
   //Data Hook
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
+  const uri = API_URL + API_KEY + "&page=";
+
   //Fetching Data Function
-  const FetchMovieData = async (uri) => {
+  const FetchMovieData = async () => {
     try {
-      const response = await fetch(uri);
+      const response = await fetch(uri + CURR_PAGE);
       const data = await response.json();
-      setData(data['results']);
+      console.log("Total Page: " + data["total_pages"]);
+      setData([...data["results"]]);
     } catch (error) {
       console.error(error);
     } finally {
@@ -24,7 +31,7 @@ export default function HomeScreen() {
   };
 
   useEffect(() => {
-    FetchMovieData(API_URL + API_KEY);
+    FetchMovieData();
   }, []);
 
   const renderItem = ({ item }) => (
@@ -36,12 +43,33 @@ export default function HomeScreen() {
         genre={item.genre_ids}
         poster={item.poster_path}
         overview={item.overview}
+        key={item.id}
       />
     </View>
   );
-  loadMore = () => {
-    alert('You Got to end');
+
+  const handleOnEndReached = async () => {
+    CURR_PAGE += 1;
+    console.log("New CUR " + CURR_PAGE);
+    if (!stopFetchMore) {
+      const response = await fetch(uri + CURR_PAGE);
+      const resdata = await response.json();
+      if (resdata["total_pages"] === TOTAL_PAGE) return;
+      setData([...data, ...resdata["results"]]);
+    }
   };
+
+  const handOnScrollTop = async () => {
+    CURR_PAGE -= 1;
+    console.log("New CUR " + CURR_PAGE);
+    if (!stopFetchMore) {
+      const response = await fetch(uri + CURR_PAGE);
+      const resdata = await response.json();
+      if (resdata["total_pages"] === 1) return;
+      setData([...data, ...resdata["results"]]);
+    }
+  };
+
   return (
     <View style={styles.cardList}>
       {isLoading ? (
@@ -49,9 +77,14 @@ export default function HomeScreen() {
       ) : (
         <FlatList
           data={data}
-          keyExtractor={({ id }) => id}
+          keyExtractor={({ id, index }) => id}
           renderItem={renderItem}
-          maxToRenderPerBatch={0}
+          onEndReached={handleOnEndReached}
+          onEndReachedThreshold={0.01}
+          onScrollBeginDrag={() => {
+            stopFetchMore = false;
+          }}
+          onScrollToTop={handOnScrollTop}
         />
       )}
     </View>
